02cce25317b4094689cdd8f4528e42feeae3f073
Rodolfo <rodolfocastro706@gmail.com>
2023-01-23 20:09:44 -0300

    clase04 terminada

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 import { addTask } from "./components/addTask.js";
-import { readTasks } from "./components/readTask.js";
+import { displayTasks } from "./components/readTask.js";
 const btn = document.querySelector('[data-form-btn]');
 
 //Arrow functions o funciones anonimas
 btn.addEventListener('click', addTask);
 
-readTasks();
+displayTasks();
